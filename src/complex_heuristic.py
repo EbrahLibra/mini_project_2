dd1c0767aa8f71a8c4c3286a562d6a818ef99217
ebrah <ebraheemalshapi@gmail.com>
2021-11-04 08:55:43 -0400

    Optimize diagonal check

--- src/complex_heuristic.py
+++ src/complex_heuristic.py
@@ -16,7 +16,6 @@ class ComplexHeuristic(BaseHeuristic):
     def calculate_value(self, board) -> int:
         # diagonal analysis
         length = len(board)
-        length = length
         x_expected_winning_criteria = 'X' * self._success_factor
         o_expected_winning_criteria = 'O' * self._success_factor
 
@@ -27,37 +26,41 @@ class ComplexHeuristic(BaseHeuristic):
             for row, row_list in enumerate(board):
                 if 0 <= column + row < length:
                     target_diagonal += row_list[column + row]
-            if x_expected_winning_criteria in target_diagonal.replace('.', 'X'):
-                x_diagonal_chances += 1
-            if o_expected_winning_criteria in target_diagonal.replace('.', 'O'):
-                o_diagonal_chances += 1
+            if len(target_diagonal) >= self._success_factor:
+                if x_expected_winning_criteria in target_diagonal.replace('.', 'X'):
+                    x_diagonal_chances += 1
+                if o_expected_winning_criteria in target_diagonal.replace('.', 'O'):
+                    o_diagonal_chances += 1
         for column in reversed(range(-(length - 1), 1)):
             target_diagonal = ''
             for row, row_list in enumerate(board):
                 if 0 <= column + row < length:
                     target_diagonal += row_list[column + row]
-            if x_expected_winning_criteria in target_diagonal.replace('.', 'X'):
-                x_diagonal_chances += 1
-            if o_expected_winning_criteria in target_diagonal.replace('.', 'O'):
-                o_diagonal_chances += 1
+            if len(target_diagonal) >= self._success_factor:
+                if x_expected_winning_criteria in target_diagonal.replace('.', 'X'):
+                    x_diagonal_chances += 1
+                if o_expected_winning_criteria in target_diagonal.replace('.', 'O'):
+                    o_diagonal_chances += 1
         for column in reversed(range(length)):
             target_diagonal = ''
             for row, row_list in enumerate(board):
                 if 0 <= column - row < length:
                     target_diagonal += row_list[column - row]
-            if x_expected_winning_criteria in target_diagonal.replace('.', 'X'):
-                x_diagonal_chances += 1
-            if o_expected_winning_criteria in target_diagonal.replace('.', 'O'):
-                o_diagonal_chances += 1
+            if len(target_diagonal) >= self._success_factor:
+                if x_expected_winning_criteria in target_diagonal.replace('.', 'X'):
+                    x_diagonal_chances += 1
+                if o_expected_winning_criteria in target_diagonal.replace('.', 'O'):
+                    o_diagonal_chances += 1
         for column in range(1, length):
             target_diagonal = ''
             for row, row_list in enumerate(reversed(board)):
                 if 0 <= column + row < length:
                     target_diagonal += row_list[column + row]
-            if x_expected_winning_criteria in target_diagonal.replace('.', 'X'):
-                x_diagonal_chances += 1
-            if o_expected_winning_criteria in target_diagonal.replace('.', 'O'):
-                o_diagonal_chances += 1
+            if len(target_diagonal) >= self._success_factor:
+                if x_expected_winning_criteria in target_diagonal.replace('.', 'X'):
+                    x_diagonal_chances += 1
+                if o_expected_winning_criteria in target_diagonal.replace('.', 'O'):
+                    o_diagonal_chances += 1
 
         x_vertical_chances = 0
         o_vertical_chances = 0
