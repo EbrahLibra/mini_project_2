04b356013d3ddb6ae256efb2ebd7f438198b9631
ebrah <ebraheemalshapi@gmail.com>
2021-11-06 22:35:03 -0400

    Fix simple heuristic function

--- src/simple_heuristic.py
+++ src/simple_heuristic.py
@@ -3,14 +3,13 @@ from base_heuristic import BaseHeuristic
 
 
 class SimpleHeuristic(BaseHeuristic):
-    def __init__(self, max=False):
-        self._max = max
+    def __init__(self):
+        pass
 
     # TODO This function is not working properly
     # We need better solution
-    def calculate_value(self, board) -> int:
-        board_size = len(board)
-        if max:
-            return -random.randint(0, board_size - 1)
+    def calculate_value(self, success_factor, max_turn=False) -> int:
+        if max_turn:
+            return -(success_factor-1)
         else:
-            return +random.randint(0, board_size - 1)
+            return success_factor-1
